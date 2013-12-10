35045f8387578bdd86dbfcd014eb045673924ed4
David Hoyes <dphoyes@gmail.com>
2013-12-10 02:42:54 +0000

    Add active nodes list to GUI

--- scripts/gui.py
+++ scripts/gui.py
@@ -10,6 +10,7 @@ import cv_bridge
 import numpy as np
 from PIL import Image
 import StringIO
+import subprocess
 
 class MainWindow(Gtk.Window):
 
@@ -23,6 +24,8 @@ class MainWindow(Gtk.Window):
         self.initialise_webcam_feed()
         self.builder.get_object("window").show()
         GLib.timeout_add(100, self.update_webcam_feed)
+        GLib.timeout_add_seconds(1, self.spinOnce)
+        self.spinOnce()
 
     def initialise_ros_ip_entry(self):
         entry = self.builder.get_object("ros_master_uri_entry")
@@ -48,25 +51,46 @@ class MainWindow(Gtk.Window):
             loader.write(ppm_image)
             self.cam_pixbuf = loader.get_pixbuf()
             loader.close()
-            self.cam_pixbuf = self.cam_pixbuf.scale_simple(800, 600, GdkPixbuf.InterpType.BILINEAR)
+            # self.cam_pixbuf = self.cam_pixbuf.scale_simple(800, 600, GdkPixbuf.InterpType.BILINEAR)
             self.builder.get_object("webcam_feed").set_from_pixbuf(self.cam_pixbuf)
         return True
 
     def initialise_webcam_feed(self):
         self.cvBridge = cv_bridge.CvBridge()
         self.ros_frame = None
-        topic = "/usb_cam/image_raw/decompressed"
+        topic = "/user_identification/video"
         rospy.Subscriber(topic, sensor_msgs.msg.Image, self.ros_camera_callback, queue_size=1)
 
     def voice_recording_start_clicked_cb(self, *args):
-        rospy.ServiceProxy('/voice_control/recording_start', std_srvs.srv.Empty)()
+        self.send_service_command('/voice_control/recording_start')
         
     def voice_recording_stop_clicked_cb(self, *args):
-        rospy.ServiceProxy('/voice_control/recording_stop', std_srvs.srv.Empty)()
+        self.send_service_command('/voice_control/recording_stop')
+
+    def send_service_command(self, service_name):
+        try:
+            rospy.ServiceProxy(service_name, std_srvs.srv.Empty)()
+        except rospy.service.ServiceException:
+            statusbar = self.builder.get_object('statusbar')
+            statusbar.push(1, "Warning: Service " + service_name + " is not available.")
+            GLib.timeout_add_seconds(1, lambda: statusbar.pop(1))
 
     def on_window_destroy(self, *args):
         Gtk.main_quit(*args)
 
-rospy.init_node('barista_bot_control_gui')
-MainWindow()
-Gtk.main()
+    def spinOnce(self, *args):
+        active_nodes = subprocess.check_output(["rosnode", "list"])
+        active_nodes_view = self.builder.get_object("active_nodes_body")
+        active_nodes_view.set_text(active_nodes)
+        return True
+    
+
+if __name__ == '__main__':
+    import signal
+    def kill_handler(*args):
+        Gtk.main_quit()
+    signal.signal(signal.SIGINT, kill_handler)
+
+    rospy.init_node('barista_bot_control_gui')
+    MainWindow()
+    Gtk.main()
